c3f161a3c6c4514649321e064eb2b34cf9bbd581
BlueBearBeer <sry990715@gmail.com>
2024-07-11 11:16:51 +0900

    체크포인트

--- src/components/Register/Contents/RegisterContents.jsx
+++ src/components/Register/Contents/RegisterContents.jsx
@@ -7,6 +7,9 @@ export default function RegisterContents() {
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
+    const [email, setEmail] = useState('');
+    const [emailDomain, setEmailDomain] = useState('naver.com');
+    const [nickname, setNickname] = useState('');
     const [idValid, setIdValid] = useState(true);
     const [passwordValid, setPasswordValid] = useState(true);
     const [passwordMatch, setPasswordMatch] = useState(true);
@@ -21,7 +24,7 @@ export default function RegisterContents() {
         return reg.test(password);
     }
 
-    function handleNameChange(e){
+    function handleNameChange(e) {
         setName(e.target.value);
     }
 
@@ -40,75 +43,90 @@ export default function RegisterContents() {
         setPasswordMatch(e.target.value === password);
     }
 
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+
+    function handleEmailDomainChange(e) {
+        setEmailDomain(e.target.value);
+    }
+
+    function handleNicknameChange(e) {
+        setNickname(e.target.value);
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
         if (!idValid || !passwordValid || !passwordMatch) {
             return;
         }
-        const formData = JSON.stringify({ name,id, password });
+        const formData = JSON.stringify({
+            name,
+            id,
+            password,
+            email: `${email}@${emailDomain}`,
+            nickname
+        });
 
-        
-        fetch("http://192.168.20.2:8090/ms3/user/insert", {
+        fetch("http://localhost:8090/ms3/user/insert", {
             method: 'POST',
             body: formData,
             headers: {
-                'Content-Type': 'application/json'
             },
+            credentials: 'include'
         }).then(response => {
             return response.json();
-        }).then(result =>{
+        }).then(result => {
             console.log(result);
         })
-        .catch(error =>
-            console.error('Error:', error));
-            
-        }
+            .catch(error => console.error('Error:', error));
+    }
 
-
-
-return (
-    <div className={styles.page}>
-        <MenuContents />
-        <div className={styles.body}>
-            <div className={styles.container}>
-                <h1 className={styles.title}>Sign-in</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className={styles.name}>
-                        <input type="text" value={name} onChange={handleNameChange} placeholder="성함을 입력해주세요" />
+    return (
+        <header>
+            <div className={styles.page}>
+                <MenuContents />
+                <div className={styles.body}>
+                    <div className={styles.container}>
+                        <h1 className={styles.title}>Sign-in</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div className={styles.name}>
+                                <input type="text" value={name} onChange={handleNameChange} placeholder="성함을 입력해주세요" />
+                            </div>
+                            <div className={styles.name}>
+                                <input type="text" value={id} onChange={handleIdChange} placeholder="아이디를 입력해주세요" />
+                            </div>
+                            {!idValid && <span className={styles.error}>*아이디는 8글자 이상 20자 이하로 알파벳 숫자 조합으로 입력하세요</span>}
+                            <div className={styles.txt_box}>
+                                <input type="password" value={password} onChange={handlePasswordChange} placeholder="패스워드를 입력해주세요" />
+                            </div>
+                            {!passwordValid && <span className={styles.error}>*암호는 숫자, 특수문자 1글자씩 포함되어야합니다.8~32글자 사이로 입력하세요</span>}
+                            <div className={styles.txt_box}>
+                                <input type="password" value={passwordCheck} onChange={handlePasswordCheckChange} placeholder="패스워드 확인" />
+                            </div>
+                            {!passwordMatch && <span className={styles.error}>*암호가 일치하지 않습니다.</span>}
+                            <div className={styles.email}>
+                                <div className={styles.email_box}>
+                                    <input type="text" value={email} onChange={handleEmailChange} placeholder="이메일을 입력해주세요" />
+                                </div>
+                                <div className={styles.email_option}>
+                                    <select name="emailDomain" value={emailDomain} onChange={handleEmailDomainChange}>
+                                        <option value="naver.com">naver.com</option>
+                                        <option value="gmail.com">gmail.com</option>
+                                        <option value="icloud.com">icloud.com</option>
+                                        <option value="kakao.com">kakao.com</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className={styles.nick}>
+                                <input type="text" value={nickname} onChange={handleNicknameChange} placeholder="닉네임을 입력해주세요" />
+                            </div>
+                            <button type="submit" id="submit" className={styles.btn_submit}>Submit</button>
+                            <button type="reset" className={styles.btn_cancel}>Cancel</button>
+                        </form>
                     </div>
-                    <div className={styles.name}>
-                        <input type="text" value={id} onChange={handleIdChange} placeholder="아이디를 입력해주세요" />
-                    </div>
-                    {!idValid && <span className={styles.error}>*아이디는 8글자 이상 20자 이하로 알파벳 숫자 조합으로 입력하세요</span>}
-                    <div className={styles.txt_box}>
-                        <input type="password" value={password} onChange={handlePasswordChange} placeholder="패스워드를 입력해주세요" />
-                    </div>
-                    {!passwordValid && <span className={styles.error}>*암호는 숫자, 특수문자 1글자씩 포함되어야합니다.8~32글자 사이로 입력하세요</span>}
-                    <div className={styles.txt_box}>
-                        <input type="password" value={passwordCheck} onChange={handlePasswordCheckChange} placeholder="패스워드 확인" />
-                    </div>
-                    {!passwordMatch && <span className={styles.error}>*암호가 일치하지 않습니다.</span>}
-                    <div className={styles.email}>
-                        <div className={styles.email_box}>
-                            <input type="text" placeholder="이메일을 입력해주세요" />
-                        </div>
-                        <div className={styles.email_option}>
-                            <select name="email">
-                                <option value="naver.com">naver.com</option>
-                                <option value="gmail.com">gmail.com</option>
-                                <option value="icloud.com">icloud.com</option>
-                                <option value="kakao.com">kakao.com</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className={styles.nick}>
-                        <input type="text" placeholder="닉네임을 입력해주세요" />
-                    </div>
-                    <button type="submit" className={styles.btn_submit} id='submit'>Submit</button>
-                    <button type="reset" className={styles.btn_cancel}>Cancel</button>
-                </form>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        </header>
+    );
 }
