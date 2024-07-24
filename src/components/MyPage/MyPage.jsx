c3ffe38c3f7eeeade40d21148dfbd6d08fff138b
TEEPUT <ciyi1594@gmail.com>
2024-07-24 12:05:05 +0900

    출석페이지 완성

--- src/components/MyPage/MyPage.jsx
+++ src/components/MyPage/MyPage.jsx
@@ -4,6 +4,7 @@ import styles from './css/MyPage.module.css';
 import Library from './contents/Library';
 import Friends from './contents/Friends';
 import MailBox from './contents/MailBox';
+import Attendance from './contents/Attendance';
 
 export default function MyPage({ setToken }) {
     const [currentPage, setCurrentPage] = useState('profile');
@@ -24,6 +25,10 @@ export default function MyPage({ setToken }) {
         setCurrentPage('mailbox');
     };
 
+    const showAttendance = () => {
+        setCurrentPage('attendance');
+    };
+
     return (
         <div className={styles.container}>
             <h1 className={styles.pageTitle}>My Page</h1>
@@ -32,12 +37,14 @@ export default function MyPage({ setToken }) {
                 <button type='button' onClick={showMyDex}>내 도감</button>
                 <button type='button' onClick={showMyFriends}>내 친구</button>
                 <button type='button' onClick={showMailbox}>메일함</button>
+                <button type='button' onClick={showAttendance}>메일함</button>
             </nav>
             <div className={styles.content}>
                 {currentPage === 'profile' && <Profile />}
                 {currentPage === 'myDex' && <Library />}
                 {currentPage === 'myFriends' && <Friends />}
                 {currentPage === 'mailbox' && <MailBox />}
+                {currentPage === 'attendance' && <Attendance />}
             </div>
         </div>
     );
