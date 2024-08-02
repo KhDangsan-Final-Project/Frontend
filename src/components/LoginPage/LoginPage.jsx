c5c5036cea6dc0c12f26b96b345aca9994361d2f
TEEPUT <ciyi1594@gmail.com>
2024-08-02 16:37:56 +0900

    리셋페이지 라우트 수정

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Login from './Login/Login';
 import styles from './css/LoginPage.module.css';
 import Register from './Register/Register';
-import PasswordResetRequestPage from './PasswdResetRequestPage/PasswordResetRequestPage';
+import PasswordResetRequestPage from './PasswdReset/PasswordResetRequestPage';
 
 export default function LoginPage({ setToken }) {
     const [formType, setFormType] = useState('login');
